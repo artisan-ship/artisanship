780218995e8cc42e4d87c461853a483c188422fd
leon <leon00albert@gmail.com>
2020-03-23 15:11:19 +0000

    change css for login and register + fix register router

--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,70 @@
 var express = require('express');
-var router = express.Router()
-var passport =require('passport');
-var User = require("../models/users");
-var Product   = require("../models/products");
+var router = express.Router();
+var passport = require('passport');
+var User = require('../models/users');
+var Product = require('../models/products');
+
+router.get('/', function(req, res) {
+    Product.find({}, function(err, foundProduct) {
+        if (err) {
+            console.log('err');
+        } else {
+            res.render('home', { products: foundProduct });
+        }
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login');
+});
+
+router.post(
+    '/login',
+    passport.authenticate('local', {
+        successRedirect: '/admin',
+        failureRedirect: '/login'
+    }),
+    function(req, res) {}
+);
 
 
 
-
-router.get("/", function(req, res){
-	
-	
-	Product.find({}, function(err, foundProduct){
-		if(err){
-			console.log("err");
-		}
-		else{
-				res.render("home",{products:foundProduct});
-
-		}
-		
-	});
-	
-
-	
-	
+router.get("/register", function(req, res){
+	res.render("register");
 })
 
 
+router.post("/register",function(req,res){
 
-router.get("/login",function(req,res){
+
 	
-	res.render("login");
-	
+	User.register(new User({username: req.body.username }), req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+			passport.authenticate("local")(req, res, function(){
+				res.redirect("/")
+			})
+		
+		
+	});
 });
 
 
+router.get('/logout', function(req, res) {
+    req.logout();
 
-
-router.post("/login",passport.authenticate("local",{
-	successRedirect : "/admin",
-	failureRedirect : "/login"
-}),function(req,res){
-
+    res.redirect('/');
 });
 
 
-router.get("/logout",function(req,res){
-	
-	req.logout();
-	
-	res.redirect("/");
-	
-});
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
 
-
-function isLoggedIn(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	}
-	
-	res.redirect("/login");
+    res.redirect('/login');
 }
 
-
-
-module.exports = router
+module.exports = router;
