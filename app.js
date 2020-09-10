bbbef1de9dfc0c3892555a4e7bc37f2a429c2ddd
Leon <leon00albert@gmail.com>
2020-09-10 11:56:58 +0400

    add webhook

--- app.js
+++ app.js
@@ -48,7 +48,39 @@ app.use(require("express-session")({
 	saveUninitialized: false
 }));
 
+// Initialize WebHooks module.
+var WebHooks = require('node-webhooks')
+ 
+// Initialize webhooks module from on-disk database
+var webHooks = new WebHooks({
+    db: './webHooksDB.json', // json file that store webhook URLs
+    httpSuccessCodes: [200, 201, 202, 203, 204], //optional success http status codes
+})
+ 
+// Alternatively, initialize webhooks module with object; changes will only be
+// made in-memory
+webHooks = new WebHooks({
+    db: {"addPost": ["http://localhost:3000/posts"]}, // just an example
+})
+ 
+// sync instantation - add a new webhook called 'shortname1'
+webHooks.add('mailparse', 'http://theartisanship.com/payload"').then(function(){
+    // done
+}).catch(function(err){
+    console.log(err)
+})
+ 
 
+ 
+// remove a single url attached to the given shortname
+// webHooks.remove('shortname3', 'http://127.0.0.1:9000/query/').catch(function(err){console.error(err);})
+ 
+// if no url is provided, remove all the urls attached to the given shortname
+// webHooks.remove('shortname3').catch(function(err){console.error(err);})
+ 
+// trigger a specific webHook
+webHooks.trigger('mailparse', {data: 123})
+ 
 
 app.use(passport.initialize());
 app.use(passport.session());
