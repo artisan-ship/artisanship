9dca149804885dca9669ca8780b26763d3c308e0
Leon <leon00albert@gmail.com>
2020-09-10 11:42:45 +0400

    add webhook for mailparser

--- app.js
+++ app.js
@@ -30,71 +30,6 @@ app.use(cookieParser('secretString'));
 
 
 
-
-
-var Imap = require('imap'),
-    inspect = require('util').inspect;
- 
-var imap = new Imap({
-  user: 'orders@theartisanship.com',
-  password: 'asorders2020',
-  host: 'imap.gmail.com',
-  port: 993,
-  tls: true
-});
- 
-function openInbox(cb) {
-  imap.openBox('INBOX', true, cb);
-}
- 
-imap.once('ready', function() {
-  openInbox(function(err, box) {
-    if (err) throw err;
-    var f = imap.seq.fetch('1:3', {
-      bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-      struct: true
-    });
-    f.on('message', function(msg, seqno) {
-      console.log('Message #%d', seqno);
-      var prefix = '(#' + seqno + ') ';
-      msg.on('body', function(stream, info) {
-        var buffer = '';
-        stream.on('data', function(chunk) {
-          buffer += chunk.toString('utf8');
-        });
-        stream.once('end', function() {
-          console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-        });
-      });
-      msg.once('attributes', function(attrs) {
-        console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-      });
-      msg.once('end', function() {
-        console.log(prefix + 'Finished');
-      });
-    });
-    f.once('error', function(err) {
-      console.log('Fetch error: ' + err);
-    });
-    f.once('end', function() {
-      console.log('Done fetching all messages!');
-      imap.end();
-    });
-  });
-});
- 
-imap.once('error', function(err) {
-  console.log(err);
-});
- 
-imap.once('end', function() {
-  console.log('Connection ended');
-});
- 
-imap.connect();
-
-
-
 app.use(flash());
 
 
@@ -163,7 +98,10 @@ const calculateOrderAmount = items => {
 	return 1400;
   };
 
-
+  app.post("/payload", (req, res) => {
+	console.log(req.body) // Call your action on the request here
+	res.status(200).end() // Responding is important
+  })
 
   app.get('/checkout', async (req, res) => {
 
