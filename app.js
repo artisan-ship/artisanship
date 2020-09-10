6d7e0ff2a851fda8473475f175eef517e8ba9e70
Leon <leon00albert@gmail.com>
2020-09-10 11:49:37 +0400

    bug fix

--- app.js
+++ app.js
@@ -101,6 +101,7 @@ const calculateOrderAmount = items => {
   app.post("/payload", (req, res) => {
 	console.log(req.body) // Call your action on the request here
 	res.status(200).end() // Responding is important
+	res.send("message"+ req.body)
   })
 
   app.get('/checkout', async (req, res) => {
