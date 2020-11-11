301933fa9c1692b85391f9112f63962379f57d55
leon <leon00albert@gmail.com>
2020-11-11 14:21:08 +0400

    add logs

--- app.js
+++ app.js
@@ -26,7 +26,31 @@ var reviewRoutes = require('./routes/reviews')
 var UserInfo = require('./models/user_info');
 const { json } = require('body-parser');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
+const winston = require('winston');
+ 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+ 
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
 const app = express();
 
 app.use(cookieParser('secretString'));
@@ -59,7 +83,7 @@ app.use(passport.session());
 passport.use(new localStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
-
+// Error Handling Helper Function
 
 
 app.use(async function(req, res, next){
@@ -224,7 +248,6 @@ var emails = "";
 
 
 
-  
 app.use(function(req, res, next) {
     res.status(404);
     res.render('404-page');
